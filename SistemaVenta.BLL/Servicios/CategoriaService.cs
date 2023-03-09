3b734afe6ea19f8a89e5ebb896a5028ced97e312
Facundo Villalobo <facundo.villalobo@valkimia.com>
2023-03-08 22:35:59 -0300

    Se agrega interfaz para el servicio de Categoria y su implementacion

---
+++ SistemaVenta.BLL/Servicios/CategoriaService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly IMapper _mapper;
+        private readonly IGenericRepository<Categoria> _categoriaRepository;
+
+        public CategoriaService(IMapper mapper, IGenericRepository<Categoria> categoriaRepository)
+        {
+            _mapper = mapper;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public async Task<List<CategoriaDTO>> GetLista()
+        {
+            try
+            {
+                var listaCategorias = await _categoriaRepository.Consultar();
+                return _mapper.Map<List<CategoriaDTO>>(listaCategorias.ToList());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
