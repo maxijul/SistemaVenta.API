46c6566b19264c2df1559adee7aaa322390c7373
Facundo Villalobo <facundo.villalobo@valkimia.com>
2023-03-08 22:02:47 -0300

    Se añade interfaz el service de Rol junto con su implementacion

---
+++ SistemaVenta.BLL/Servicios/RolService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class RolService : IRolService
+    {
+        private readonly IGenericRepository<Rol> _rolRepository;
+        private readonly IMapper _mapper;
+
+        public RolService(IGenericRepository<Rol> rolRepository, IMapper mapper)
+        {
+            _rolRepository = rolRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<RolDTO>> GetLista()
+        {
+            try
+            {
+                var listaRoles = await _rolRepository.Consultar();
+                return _mapper.Map<List<RolDTO>>(listaRoles.ToList());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
